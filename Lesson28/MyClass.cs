ad44eee1dbdcb87d9dadc3aea600c37c6cf843ea
Вадим Шванов <adm.shwan@gmail.com>
2019-02-21 21:10:16 +0300

    Добавьте файлы проекта.

---
+++ Lesson28/MyClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lesson28
+{
+    class MyClass : IDisposable
+    {
+        public MyClass() { }
+
+        ~MyClass() { }
+
+        public void Dispose()
+        {
+            GC.Collect();
+        }
+
+
+    }
+}
---
+++ Lesson28/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Lesson28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Домашнее задание
+            // Изучить класс GC
+
+            //GC.
+
+            Console.WriteLine(GC.GetTotalMemory(false));
+
+            for(int i = 0; i < 10000000; i++)
+            {
+                var obj = (object)i;
+                int j = (int)obj;
+            }
+
+            Console.WriteLine(GC.GetTotalMemory(false));
+
+            using (var c = new MyClass())
+            {
+
+            }
+
+            Console.WriteLine(GC.GetTotalMemory(false));
+
+            GC.Collect();
+
+            Console.WriteLine(GC.GetTotalMemory(false));
+
+            Console.ReadLine();
+        }
+    }
+}
